8f93e07c9573d1419db7ea987468974022ac5d78
georgesarell <36608394+georgesarell@users.noreply.github.com>
2018-02-22 11:28:43 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.set('views', __dirname + '/views');
 app.get('/', function (req, res) {
     res.render('index', {
         content: null,
-        searchterm: 'Curiosiki',
+        searchterm: 'Curiousiki',
         digValue: null
     });
 });
@@ -25,7 +25,7 @@ app.post("/", function (req, res) {
     formBody(req, {}, function (err, body) {
         var query = body.frmSearch;
 
-        if (body.frmSearch == null || body.frmSearch === "" || body.frmSearch.toUpperCase() === "CURIOSIKI") {
+        if (body.frmSearch == null || body.frmSearch === "" || body.frmSearch.toUpperCase() === "CURIOUSIKI") {
             res.redirect('/about');
             return;
         }
@@ -42,7 +42,7 @@ app.post("/", function (req, res) {
 app.get('/about', function (req, res) {
     res.render('about', {
         content: null,
-        searchterm: 'Curiosiki',
+        searchterm: 'Curiousiki',
         digValue: null
     });
 });
@@ -51,7 +51,7 @@ app.get('/about', function (req, res) {
 app.get('/blank', function (req, res) {
     res.render('index', {
         content: null,
-        searchterm: 'Curiosiki',
+        searchterm: 'Curiousiki',
         digValue: null
     });
 });
